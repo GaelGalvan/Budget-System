ff0254a7fd0f51c8746d0ebe511cc120d9cce6f9
GaelGalvan <gael.galvan01@utrgv.edu>
2023-04-19 14:33:37 -0500

    Added Apperance option

--- tab2.py
+++ tab2.py
@@ -4,7 +4,6 @@ import os as os
 from budgeter_2 import *
 from customtkinter import *
 
-set_appearance_mode("Dark")
 set_default_color_theme("green")
 
 
@@ -35,6 +34,14 @@ class LeftTabbing(CTkTabview):
 
         # Each tab configuration
         CTkLabel(master = self.tab("Home")).grid(row = 0, column = 0, padx = (50,0))
+        self.appearance_option = CTkOptionMenu(master = self.tab("Settings"), values=["Default", "Dark", "Light"], command=self.changeAppearance)
+        self.appearance_option.grid(row = 0, column = 1, padx = (50,0))
+    def changeAppearance (self, mode : str):
+        if mode == "Default":
+            set_appearance_mode("System")
+        else:
+            set_appearance_mode(mode)
+
 class RightTabbing(CTkTabview):
     def __init__(self, master, width, **kwargs):
         super().__init__(master, width, height, **kwargs)
@@ -42,8 +49,9 @@ class RightTabbing(CTkTabview):
         #All tabs
         self.add("Purpose")
         self.add("More Info")
-    
 
+        # Each tab configuration
+        
 if __name__ == "__main__":
     app = StartTab()
     app.mainloop()
