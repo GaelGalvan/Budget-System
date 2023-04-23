9d410235ee7bbe5d373dcdc25762f68be9a18c4d
GaelGalvan <gael.galvan01@utrgv.edu>
2023-04-23 00:43:56 -0500

    tab2 finish, login began

--- tab2.py
+++ tab2.py
@@ -10,30 +10,41 @@ class HomeTab(CTk):
     def __init__(self):
         super().__init__()
         self.title("Budget Home")
-        self.geometry("1100x580")
+        self.geometry("1200x680")
         self.grid_columnconfigure((0,1,2,3), weight=1)
         self.grid_rowconfigure((1,3), weight=1)
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
 
         # Home Tab Before Entering
+
+        # Left Frame
         self.welcomeLabel = CTkLabel(master = self, text="Welcome To The Budgeter", font = CTkFont(size = 30, weight = "bold")).grid(row = 0, column = 1, sticky = "nsew")
         self.tabLeftFrame = CTkFrame(master = self, width = 250).grid(row = 1, column = 0, padx = (0,20), sticky = "nsew")
         self.tabView = LeftTabbing(master = self.tabLeftFrame, width= 300).grid(row = 1, column = 0, padx = (0,20), sticky = "nsew")
+        
+        # Mid Frame with Image
+        self.midFrame = CTkFrame(master = self, width = 100,height = 100, fg_color="transparent").grid(row = 1, column = 2, padx = (0,20), sticky = "nsew")
+        self.HomeImage = CTkImage(Image.open(os.path.join(image_path, "Budgeter.png")), size=(350, 350))
+        self.HomeLabel = CTkLabel(master = self.midFrame, text="", image=self.HomeImage)
+        self.HomeLabel.grid(row = 1, column = 1, padx = (70,0))
+
+        # Right Frame
         self.tabRightFrame = CTkFrame(master = self, width = 250).grid(row = 1, column = 5, padx = (20,0), sticky = "nsew")
         self.tabView2 = RightTabbing(master = self.tabLeftFrame, width= 300).grid(row = 1, column = 5, padx = (20,0), sticky = "nsew")
 
         #Start Button  
-        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
-        self.startImage = CTkImage(light_image=Image.open(os.path.join(image_path, "startButton2.png")),
-                                                 dark_image=Image.open(os.path.join(image_path, "StartButton.png")), size=(150, 150))
-
+        self.startImage = CTkImage(dark_image=Image.open(os.path.join(image_path, "StartButton.png")), size=(150, 150))
         self.startFrame = CTkFrame(master = self, width = 0, height = 50)
         self.startFrame.grid_columnconfigure(2, weight = 1)
         self.startFrame.grid(row = 3, column = 0, columnspan = 10, sticky = "NSEW")
-        self.startButton = CTkButton(master = self.startFrame,text="",image=self.startImage, command = self.start, width=100, fg_color="transparent")
-        self.startButton.grid(row = 3, column = 2, padx = 20, pady = 20)
+        self.startButton = CTkButton(master = self.startFrame,text="",image=self.startImage, command = self.websearch, width=100, fg_color="transparent")
+        self.startButton.grid(row = 0, column = 2, padx = 20, pady = 20)
+
     def start(self):
         HomeTab.quit
         StartTab(isfs, theme, slider, appearance_window)
+    def websearch(self):
+        webbrowser.open_new(r"gaelgalvan12@gmail.com")
 
 # Class which will hold tabs
 class LeftTabbing(CTkTabview):
@@ -48,7 +59,7 @@ class LeftTabbing(CTkTabview):
         self.textbox = CTkTextbox(master = self.tab("Home"), width=250, height = 350)
         
         self.textbox.grid(row=0, column = 0, padx=(20, 0), pady=(20, 0), sticky="nsew")
-        with open('Home_Text.txt', 'r' ) as x:
+        with open('Text\\Home_Text.txt', 'r' ) as x:
             Text = x.read()
             self.textbox.insert("0.0",Text)
         self.textbox.configure(state = "disabled")
@@ -108,8 +119,29 @@ class RightTabbing(CTkTabview):
         self.add("Purpose")
         self.add("More Info")
 
-        # Each tab configuration
+        
+        # Purpose Configuration
+        self.textbox = CTkTextbox(master = self.tab("Purpose"), width=250, height = 350)
+        
+        self.textbox.grid(row=0, column = 0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        with open('Text\\Purpose_Text.txt', 'r' ) as x:
+            Text = x.read()
+            self.textbox.insert("0.0",Text)
+        self.textbox.configure(state = "disabled")
+        # More Info Configuration
+        CTkLabel(master = self.tab("More Info"), text = "Contact Us", font = CTkFont(size = 15)).grid(row = 0, column = 1, padx = (0,75), pady = (20,0))
 
+        CTkLabel(master = self.tab("More Info"), text = "Gael Galvan:", font = CTkFont(size = 15)).grid(row = 1, column = 0, padx = (40,0), pady = (20,0))
+        CTkLabel(master = self.tab("More Info"), text="gaelgalvan12@gmail.com",font= CTkFont(size=15, underline=True)).grid(row = 1, column = 1, padx = (10,0), pady = (20,0))
+
+        CTkLabel(master = self.tab("More Info"), text = "Angel Escobedo:", font = CTkFont(size = 15)).grid(row = 2, column = 0, padx = (40,0), pady = (20,0))
+        CTkLabel(master = self.tab("More Info"), text="angel.escobedo03@utrgv.edu",font= CTkFont(size=15, underline=True)).grid(row = 2, column = 1, padx = (10,0), pady = (20,0))
+
+        CTkLabel(master = self.tab("More Info"), text = "Diego Esparza:", font = CTkFont(size = 15)).grid(row = 3, column = 0, padx = (40,0), pady = (20,0))
+        CTkLabel(master = self.tab("More Info"), text="testemail123@gmail.com",font= CTkFont(size=15, underline=True)).grid(row = 3, column = 1, padx = (10,0), pady = (20,0))
+
+        CTkLabel(master = self.tab("More Info"), text = "David Garza:", font = CTkFont(size = 15)).grid(row = 4, column = 0, padx = (40,0), pady = (20,0))
+        CTkLabel(master = self.tab("More Info"), text="dgarz321@gmail.com",font= CTkFont(size=15, underline=True)).grid(row = 4, column = 1, padx = (10,0), pady = (20,0))
 class StartTab(CTk):
     def __init__(self, isFullScreen, CTheme, slider, appearance):
         super().__init__()
@@ -121,4 +153,5 @@ class StartTab(CTk):
 
 if __name__ == "__main__":
     app = HomeTab()
+    app.resizable(False,False)
     app.mainloop()
